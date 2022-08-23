94b6cbcb6051d198c0f34b2668649cd5c0d97a5f
Aleksandr Serenko <fafnur@yandex.ru>
2022-08-23 16:13:58 +0700

    [Config] Change app structure

---
+++ apps/icons/src/app/app.routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled',
+      initialNavigation: 'enabledBlocking',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
