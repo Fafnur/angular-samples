77d1371560fca19b3544bcb86bca789f8b293a50
Aleksandr Serenko <fafnur@yandex.ru>
2022-10-25 08:01:47 +0600

    [redux][ngrx] Integration ui kit with ngrx page

--- libs/redux/ngrx/posts/page/src/lib/page.module.ts
+++ libs/redux/ngrx/posts/page/src/lib/page.module.ts
@@ -5,12 +5,23 @@ import { PostStateModule } from '@angular-samples/redux/ngrx/posts/state';
 import { PostsLastModule } from '@angular-samples/redux/ngrx/posts/ui/last';
 import { PostsPopularModule } from '@angular-samples/redux/ngrx/posts/ui/popular';
 import { PostsPromoModule } from '@angular-samples/redux/ngrx/posts/ui/promo';
+import { ContainerModule } from '@angular-samples/ui/container';
+import { GridModule } from '@angular-samples/ui/grid';
 
 import { PageComponent } from './page.component';
 import { PageRoutingModule } from './page-routing.module';
 
 @NgModule({
-  imports: [CommonModule, PageRoutingModule, PostStateModule, PostsLastModule, PostsPopularModule, PostsPromoModule],
+  imports: [
+    CommonModule,
+    PageRoutingModule,
+    PostStateModule,
+    PostsLastModule,
+    PostsPopularModule,
+    PostsPromoModule,
+    GridModule,
+    ContainerModule,
+  ],
   declarations: [PageComponent],
 })
 export class PageModule {}
