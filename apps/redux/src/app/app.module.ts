5ba1e66f4012fba312a3d3b7a1ce5b49f74a7730
Aleksandr Serenko <fafnur@yandex.ru>
2022-10-25 21:49:33 +0600

    [hammer] Adding hammer js

--- apps/redux/src/app/app.module.ts
+++ apps/redux/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HammerModule } from '@angular-samples/core/hammer';
 import { RootStoreModule } from '@angular-samples/redux/ngrx/store/root';
 import { LayoutModule } from '@angular-samples/ui/layout';
 
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, RootStoreModule, LayoutModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, RootStoreModule, LayoutModule, HammerModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
