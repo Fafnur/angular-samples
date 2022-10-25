480ea94dd3143787d29af27fcf4a14cb2945c9cb
Aleksandr Serenko <fafnur@yandex.ru>
2022-10-25 07:42:54 +0600

    [redux][ngrx] Adding root store

--- apps/redux/src/app/app.module.ts
+++ apps/redux/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RootStoreModule } from '@angular-samples/redux/ngrx/store/root';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'redux' }), BrowserAnimationsModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, RootStoreModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ libs/redux/ngrx/store/root/README.md
@@ -0,0 +1,7 @@
+# redux-ngrx-store-root
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test redux-ngrx-store-root` to execute the unit tests.
