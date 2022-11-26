1e10f503d8ab3364f429c029d626411da257d06f
Aleksandr Serenko <fafnur@yandex.ru>
2022-11-26 09:17:12 +0700

    [vendors] Fix material

--- libs/redux/posts/ui/create/src/lib/post-create.component.ts
+++ libs/redux/posts/ui/create/src/lib/post-create.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 import { PostCreateDialogComponent } from './post-create-dialog/post-create-dialog.component';
 
--- libs/redux/posts/ui/create/src/lib/post-create.module.ts
+++ libs/redux/posts/ui/create/src/lib/post-create.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
 
 import { PostCreateComponent } from './post-create.component';
 import { PostCreateDialogModule } from './post-create-dialog/post-create-dialog.module';
