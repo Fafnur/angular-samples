6f6f350a5fc5576519ae1703c503192c2b791ebb
Aleksandr Serenko <fafnur@yandex.ru>
2023-07-02 19:38:13 +0700

    [mts] Moving nx welcome to lib

--- apps/module-to-standalone/src/app/app.module.ts
+++ apps/module-to-standalone/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
   providers: [],
   bootstrap: [AppComponent],
--- libs/module-to-standalone/home/page/src/lib/home-page.module.ts
+++ libs/module-to-standalone/home/page/src/lib/home-page.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: NxWelcomeComponent,
+      },
+    ]),
+  ],
+  declarations: [NxWelcomeComponent],
 })
 export class HomePageModule {}
