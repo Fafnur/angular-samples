db222361ad5b9c0d60e832566a55af993931afda
Aleksandr Serenko <fafnur@yandex.ru>
2023-07-03 17:05:58 +0700

    [mts] Removing animations

--- apps/module-to-standalone/src/app/app.config.browser.ts
+++ apps/module-to-standalone/src/app/app.config.browser.ts
@@ -1,10 +1,9 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { appConfig } from './app.config';
 
 const browserConfig: ApplicationConfig = {
-  providers: [provideAnimations()],
+  providers: [],
 };
 
 export const config = mergeApplicationConfig(appConfig, browserConfig);
